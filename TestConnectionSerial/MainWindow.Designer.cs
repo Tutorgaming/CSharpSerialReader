7e6b3dd1240046a5242a9e21334bd365e5ee390b
iTUTOR <tutorgaming@gmail.com>
2020-05-25 01:35:51 +0700

    Initial Commit

---
+++ TestConnectionSerial/MainWindow.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace TestConnectionSerial
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.serialComboBox = new System.Windows.Forms.ComboBox();
+            this.refreshSerialButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialComboBox
+            // 
+            this.serialComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.serialComboBox.FormattingEnabled = true;
+            this.serialComboBox.Location = new System.Drawing.Point(6, 19);
+            this.serialComboBox.Name = "serialComboBox";
+            this.serialComboBox.Size = new System.Drawing.Size(121, 21);
+            this.serialComboBox.TabIndex = 0;
+            // 
+            // refreshSerialButton
+            // 
+            this.refreshSerialButton.Location = new System.Drawing.Point(6, 46);
+            this.refreshSerialButton.Name = "refreshSerialButton";
+            this.refreshSerialButton.Size = new System.Drawing.Size(121, 23);
+            this.refreshSerialButton.TabIndex = 1;
+            this.refreshSerialButton.Text = "Refresh Serial";
+            this.refreshSerialButton.UseVisualStyleBackColor = true;
+            this.refreshSerialButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(6, 75);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(121, 23);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.disconnectButton);
+            this.groupBox1.Controls.Add(this.connectButton);
+            this.groupBox1.Controls.Add(this.serialComboBox);
+            this.groupBox1.Controls.Add(this.refreshSerialButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 189);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Connection";
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Location = new System.Drawing.Point(6, 104);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(121, 23);
+            this.disconnectButton.TabIndex = 3;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(154, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(480, 188);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Serial Console";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 18);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(468, 164);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(13, 208);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(621, 300);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 519);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox serialComboBox;
+        private System.Windows.Forms.Button refreshSerialButton;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ TestConnectionSerial/MainWindow.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestConnectionSerial
+{
+    public partial class MainWindow : Form
+    {
+        public string current_port;
+        public SerialReaderThread thread = null;
+        public bool text_reading = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string[] ListSerialPorts()
+        {
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+            // Display each port name to the console.
+            //foreach (string port in ports)
+            //{
+            //    Console.WriteLine("- " + port);
+            //}
+            return ports;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // FORM 1 LOADED
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] ports = ListSerialPorts();
+            serialComboBox.Items.Clear();    
+            foreach(string item in ports)
+            {
+                serialComboBox.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Object selectedItem = serialComboBox.SelectedItem;
+            string current_port = selectedItem.ToString();
+            //SerialPort serial_port = new SerialPort(current_port, 9600);
+            //serial_port.Open();
+            richTextBox1.Text += "---------- SERIAL READ THREAD STARTING ----------" + "\n";
+            if (thread == null)
+            {
+                thread = new SerialReaderThread(current_port);
+                thread.DataReceived += ThreadDataReceived;
+                thread.Start();
+            }
+            text_reading = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (thread != null)
+            {
+                richTextBox1.Text += "---------- SERIAL READ THREAD END REQUEST ----------" + "\n";
+                richTextBox1.Text += "---------- WAITING ----------" + "\n";
+                thread.Close();
+                thread = null;
+                richTextBox1.Text += "---------- SERIAL READ THREAD ENDED ----------" + "\n";
+            }
+        }
+
+        // SERIAL Data Receiver (From the Emitter)
+        // https://www.codeproject.com/Answers/228189/serial-port-using-threading#answer2
+        void ThreadDataReceived(object s, EventArgs e)
+        {
+            // Note: this method is called in the thread context, thus we must
+            // use Invoke to talk to UI controls. So invoke a method on our
+            // thread.
+            if (text_reading) BeginInvoke(new EventHandler<DataEventArgs>(ThreadDataReceivedSync), new object[] { s, e });
+        }
+
+        void ThreadDataReceivedSync(object s, DataEventArgs et)
+        {
+            // Handle Data Received by the Serial Thread
+            if (thread != null)
+            {
+                richTextBox1.Text += et.Data + "\n";
+            }
+        }
+    }
+
+    // Serial Class and Data Emitter
+    public class DataEventArgs : EventArgs
+        // DATA CLASS WHICH IS USE TO SEND DATA TO GUI
+        // start with a string Data 
+        // Graph might be double[] ? 
+    {
+        public string Data { get; private set; }
+
+        public DataEventArgs(string data) { 
+            Data = data; 
+        }
+    }
+    public class SerialReaderThread
+    {
+        private Thread t;
+        private string port;
+        private SerialPort mySerialPort;
+        public SerialReaderThread(string port)
+        {
+            this.port = port;
+            t = new Thread(RunMethod);
+        }
+
+        public void Start() { t.Start(); }
+
+        // note: this event is fired in the background thread
+        public event EventHandler<DataEventArgs> DataReceived;
+
+        // Flag determined that the spinner is running
+        private bool running = false;
+        public void Close() { 
+            running = true;
+            t.Join();
+        }
+        private void RunMethod()
+        {
+            // I'll just believe that this is correct
+            mySerialPort = new SerialPort(this.port);
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.Open();
+
+            while (!running)
+            {
+                // CUSTOM DATA TYPE CREATE HERE BY CONVERT string line into other
+                // Read the fcking serial
+                string line = mySerialPort.ReadLine();
+                // Emit string line by Data Event
+                if (DataReceived != null) DataReceived(this, new DataEventArgs(line));
+            }
+            mySerialPort.Close();
+            Console.WriteLine("Closed Serial Port : " + this.port);
+        }
+    }
+
+   
+}
